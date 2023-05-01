6d14a527848f06c72e54a3bdd3f3cf39941b7dea
isnendyan <isnendyankp@gmail.com>
2023-05-01 07:19:01 +0700

    Validate Form: add EventListener submit

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
+function processFormData(e) {
+    console.log(e);
+}
+
+
+// Event Listener
+form.addEventListener('submit', processFormData);
