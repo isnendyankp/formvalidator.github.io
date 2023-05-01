f96e71061a2eb1236e391951c923f816370c42fa
isnendyan <isnendyankp@gmail.com>
2023-05-01 07:24:15 +0700

    Validate Form: add validateForm with check true/false

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
+let isValid = false;
+
+function validateForm() {
+    // Use HTML constraint API to check form validity
+    isValid = form.checkValidity();
+    console.log(isValid);
+}
+
 function processFormData(e) {
     e.preventDefault();
-    console.log(e);
+    // Validate Form
+    validateForm();
 }
 
 
