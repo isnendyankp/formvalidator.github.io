ca387e2fb45608e9954021091ca8f77a01d7dbaf
isnendyan <isnendyankp@gmail.com>
2023-05-02 07:43:27 +0700

    Validate password & store data: create storeFormData

--- script.js
+++ script.js
@@ -43,6 +43,18 @@ function validateForm() {
   }
 }
 
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
+}
+
 function processFormData(e) {
     e.preventDefault();
     // Validate Form
