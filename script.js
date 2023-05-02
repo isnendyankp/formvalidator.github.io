48b79a617c2506a9d8a41c431617dec7c01977f7
isnendyan <isnendyankp@gmail.com>
2023-05-02 07:46:27 +0700

    Validate password & store data: submit form if valid on processFormData

--- script.js
+++ script.js
@@ -59,6 +59,10 @@ function processFormData(e) {
     e.preventDefault();
     // Validate Form
     validateForm();
+    // Submit Form if Valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
 
 
